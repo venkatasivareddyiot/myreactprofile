e80d3e1d431219762019af8ee74896071b487978
Venkata Siva Reddy Tadi <venkatasivareddyiot@gmail.com>
2020-04-28 17:04:44 +0530

    new updates

---
+++ build/precache-manifest.f93e17bcd4126f1e7291a2956dc90cba.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9aa69cd21062d3512dfd823e7cfdb2",
+    "url": "/myreactprofile/index.html"
+  },
+  {
+    "revision": "5729f27a0bd975cae9b8",
+    "url": "/myreactprofile/static/css/main.79b28cce.chunk.css"
+  },
+  {
+    "revision": "5a4f00ff3a0cd26bcc74",
+    "url": "/myreactprofile/static/js/2.1c65df27.chunk.js"
+  },
+  {
+    "revision": "5729f27a0bd975cae9b8",
+    "url": "/myreactprofile/static/js/main.5a659018.chunk.js"
+  },
+  {
+    "revision": "261ccb74558064daf5e4",
+    "url": "/myreactprofile/static/js/runtime~main.45907259.js"
+  },
+  {
+    "revision": "9584ffd2adbd0d4595fef0070605b889",
+    "url": "/myreactprofile/static/media/8051.9584ffd2.png"
+  },
+  {
+    "revision": "bf9aa6bcfb6988ce78e2335ffc3ea158",
+    "url": "/myreactprofile/static/media/altium.bf9aa6bc.png"
+  },
+  {
+    "revision": "eb32dd98dda6f1a5dc561d0e6db11f0f",
+    "url": "/myreactprofile/static/media/arduino.eb32dd98.jpg"
+  },
+  {
+    "revision": "452739fe880406db3c096b86790ce732",
+    "url": "/myreactprofile/static/media/arm.452739fe.jpg"
+  },
+  {
+    "revision": "74be8ed811c1af68e86d558d5c4a8501",
+    "url": "/myreactprofile/static/media/conta.74be8ed8.gif"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/myreactprofile/static/media/csslogo.04cf122b.png"
+  },
+  {
+    "revision": "1ef14c2947610996c034d8936ffc4283",
+    "url": "/myreactprofile/static/media/ds.1ef14c29.jpg"
+  },
+  {
+    "revision": "3a431a93c33adf4f325cfbd1547c8149",
+    "url": "/myreactprofile/static/media/html5logo.3a431a93.jpg"
+  },
+  {
+    "revision": "9a564909167d43a5024e2e3185d46612",
+    "url": "/myreactprofile/static/media/img-1.9a564909.png"
+  },
+  {
+    "revision": "ee13dd8df262883ca62ae0212c7b03f3",
+    "url": "/myreactprofile/static/media/mail.ee13dd8d.jpg"
+  },
+  {
+    "revision": "7cf0c98a6af462613dd367d37959833d",
+    "url": "/myreactprofile/static/media/mit logo.7cf0c98a.png"
+  },
+  {
+    "revision": "4115a31ba2252c98b5eeebe5a533487b",
+    "url": "/myreactprofile/static/media/ml.4115a31b.jpg"
+  },
+  {
+    "revision": "b0d212a18cf7e33413ed1d4d7bd1725f",
+    "url": "/myreactprofile/static/media/pcb.b0d212a1.jpg"
+  },
+  {
+    "revision": "f93fb837f0742ec0cc4af2296ef7d295",
+    "url": "/myreactprofile/static/media/pic.f93fb837.jpg"
+  },
+  {
+    "revision": "617ecb4da092638776eda9f2beb6fc0e",
+    "url": "/myreactprofile/static/media/profile-1.617ecb4d.jpg"
+  },
+  {
+    "revision": "c290a443b3f240732252a813d4a6cf65",
+    "url": "/myreactprofile/static/media/pythonimg.c290a443.png"
+  },
+  {
+    "revision": "191651a1440ab873cf8fc9c0f12aca19",
+    "url": "/myreactprofile/static/media/s4a.191651a1.png"
+  },
+  {
+    "revision": "bb52016c76a1111ed690b5ae0c045748",
+    "url": "/myreactprofile/static/media/st.bb52016c.jpg"
+  },
+  {
+    "revision": "434d41ac7aec3e5d435d6971996f635e",
+    "url": "/myreactprofile/static/media/w-1.434d41ac.jpeg"
+  },
+  {
+    "revision": "0cd9205a3834555c2036765f081b3e4c",
+    "url": "/myreactprofile/static/media/w-2.0cd9205a.jpeg"
+  },
+  {
+    "revision": "ab2cb1d366b311ce42c4b9fe5e8c9b58",
+    "url": "/myreactprofile/static/media/w-3.ab2cb1d3.jpeg"
+  },
+  {
+    "revision": "22af121cfa29be85dc7a023da0d621f9",
+    "url": "/myreactprofile/static/media/w-4.22af121c.jpeg"
+  },
+  {
+    "revision": "dfe6b6366fb7378fe2f24b0ad9379086",
+    "url": "/myreactprofile/static/media/w-5.dfe6b636.jpeg"
+  },
+  {
+    "revision": "37ea8cb239b75f9aab23d21c8e3056b9",
+    "url": "/myreactprofile/static/media/w-6.37ea8cb2.jpeg"
+  },
+  {
+    "revision": "d43c02f408de4c793de25c70e66f6232",
+    "url": "/myreactprofile/static/media/w-7.d43c02f4.jpeg"
+  },
+  {
+    "revision": "6aba2988c676d1fb5bb903bce9cf6d9a",
+    "url": "/myreactprofile/static/media/w-8.6aba2988.jpeg"
+  },
+  {
+    "revision": "9636e9d82b0a039456d5a9e0349b5a8e",
+    "url": "/myreactprofile/static/media/w-9.9636e9d8.jpeg"
+  },
+  {
+    "revision": "deb493608dae9e533ef6b6894095b797",
+    "url": "/myreactprofile/static/media/yt1.deb49360.png"
+  }
+]);
