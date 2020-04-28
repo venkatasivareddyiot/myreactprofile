f8197876f6bc5482502ef8cb612c680ae7a3b520
Venkata Siva Reddy Tadi <venkatasivareddyiot@gmail.com>
2020-04-28 17:16:46 +0530

    updates

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -22,29 +22,29 @@ class Portfolio extends Component {
                   <h2>Glimpse from some of My WorkShops.</h2>
 
                     <div className="container">
-                        <a><img src={w1} width="700" height="400" class="responsive"/><p>Summer program at Pragathi Engneering College</p></a>
-                        <a><img src={w2} width="700" height="400"  class="responsive"/><p> Iot WorkShop at gudlavalleru Engneering College</p></a>
+                        <a><img src={w1} width="700" height="400" /><p>Summer program at Pragathi Engneering College</p></a>
+                        <a><img src={w2} width="700" height="400"  /><p> Iot WorkShop at gudlavalleru Engneering College</p></a>
                         
                     </div>
                     <div className="container">
-                        <a><img src={w3} width="700" height="400"  class="responsive"/><p>Embedded Systems Workshop at KSRM</p></a>
-                        <a><img src={w4} width="700" height="400"  class="responsive"/><p>IoT with Node MCU at VVIT</p></a>
+                        <a><img src={w3} width="700" height="400"  /><p>Embedded Systems Workshop at KSRM</p></a>
+                        <a><img src={w4} width="700" height="400"  /><p>IoT with Node MCU at VVIT</p></a>
                         
                     </div>
                     <div className="container">
-                        <a><img src={w5} width="700" height="400"  class="responsive"/><p>FDP on IoT at KHIT</p></a>
-                        <a><img src={w6} width="700" height="400"  class="responsive"/><p> Iot WorkShop at Pragathi Engneering College</p></a>
+                        <a><img src={w5} width="700" height="400"  /><p>FDP on IoT at KHIT</p></a>
+                        <a><img src={w6} width="700" height="400"  /><p> Iot WorkShop at Pragathi Engneering College</p></a>
                         
                     </div>
                      <div className="container">
-                        <a><img src={w7} width="700" height="400"  class="responsive"/><p>Iot Workshop  at KORM</p></a>
-                        <a><img src={w8} width="700" height="400"  class="responsive"/><p> Embedded Systems WorkShop at IIIT Nuzividu</p></a>
+                        <a><img src={w7} width="700" height="400"  /><p>Iot Workshop  at KORM</p></a>
+                        <a><img src={w8} width="700" height="400" /><p> Embedded Systems WorkShop at IIIT Nuzividu</p></a>
                         
                     </div>
 
 
                     <div className="container">
-                    <a><img src={w9} width="700" height="400"  class="responsive"/><p> Iot WorkShop at Anantha Lakshmi Engneering College</p></a>
+                    <a><img src={w9} width="700" height="400"  /><p> Iot WorkShop at Anantha Lakshmi Engneering College</p></a>
                         
                     </div>
                        
